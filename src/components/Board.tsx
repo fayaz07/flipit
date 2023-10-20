eb0add5feecd79541b59664b6e406f43cc1767be
Mohammad Fayaz <fayazfz07@gmail.com>
2023-10-20 20:38:10 +0530

    remove unused import

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Card from "./Card";
 import Question from "../Question";
 import "./Board.css";
